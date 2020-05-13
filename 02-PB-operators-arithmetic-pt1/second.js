67672a1169caa74e51954cdcd5a53acde9d7d1ea
LenaIT <olena.denysova@gmail.com>
2020-05-13 15:10:14 +0200

    second ex done

---
+++ 02-PB-operators-arithmetic-pt1/second.js
@@ -0,0 +1,55 @@
+//task 0
+let x = 7;
+let y = 38;
+
+console.log(x);
+console.log(y, typeof (y));
+
+//task 1
+console.log(x + y);
+
+//task 2
+console.log(y - x);
+console.log(x - y);
+
+//task 3
+console.log(x * y);
+
+//task 4
+console.log(x / y);
+
+//5
+let z = 10;
+let resultOne = x * y / z;
+console.log(resultOne);
+
+//5
+let a = 15;
+let b = 9;
+console.log(a / b);
+
+//6
+let c = 20;
+let resultTwo = (a + b) * c;
+console.log(resultTwo);
+
+//7 Increment
+a++;
+console.log(a); //16 */
+
+/* ++a;
+console.log(a); //17 */
+
+//8 Decrement
+b--;
+console.log(b); //8
+
+/* --b;
+console.log(b); //7 */
+
+//9
+let d = a - b;
+console.log(d + c);
+
+//10
+console.log(resultOne % resultTwo);
