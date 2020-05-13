428d0125408a6bb60c59ec43347afb25f9e9702c
LenaIT <olena.denysova@gmail.com>
2020-05-13 09:35:28 +0200

    first ex done

---
+++ PB-language-statements/first.js
@@ -0,0 +1,11 @@
+console.log('Hello, World!');
+//console.log('This is my first js file');
+
+//1.node npm 2.node first.js     ctrl+shift+7
+
+/* Remove a parenthesis from the first statement and run the code. Does your statement print?
+no. 
+
+Is there an error? If so, what kind of error is it?
+Yes, SyntaxError: Unexpected string
+*/
