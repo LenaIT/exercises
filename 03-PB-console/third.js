54372bfa4fbb114c322a60fa88e37b4cf13f1974
LenaIT <olena.denysova@gmail.com>
2020-05-14 11:23:35 +0200

    ex3 done

---
+++ 03-PB-console/third.js
@@ -0,0 +1,23 @@
+//1
+console.log("this is an exercise in console logging");
+
+//2
+let myFirstVariable = "this is an exercise in console logging";
+console.log(myFirstVariable);
+
+//3
+let firstName = 'Lena';
+let lastName = 'Riemer';
+console.log(firstName, lastName);
+
+//4
+let profileFirstName = 'Brendan';
+    profileLastName = 'Eich ';
+    profileAge = '58';
+    profileProfession = 'technologist and creator of the JavaScript programming language.';
+    profileCityBorn = 'Pittsburgh, Pennsylvania, U.S.';
+    
+console.log(profileFirstName, profileLastName, profileAge, profileCityBorn, profileProfession);
+
+//5
+console.log(profileFirstName, profileLastName + "is " + profileAge + " years old. He is famous as a " + profileProfession, profileFirstName, profileLastName + "was born in " + profileCityBorn);
