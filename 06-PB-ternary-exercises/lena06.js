f3df301408e63eee21d2df57f8c76c3fca366edc
LenaIT <olena.denysova@gmail.com>
2020-05-18 11:11:20 +0200

    Lena ex6 done

---
+++ 06-PB-ternary-exercises/lena06.js
@@ -0,0 +1,21 @@
+"use strict";
+
+//1
+let isDog = "dog";
+(isDog === "dog") ? console.log('pat, pat'): console.log('find me a dog to pat!');
+
+//2
+let speedCheck = 63;
+(speedCheck >= 50) ? console.log("you're going too fast!"): console.log("You're driving below the speed limit, Oma");
+
+//3
+let age = 15;
+(age <= 16) ? console.log("serve butter beer"): console.log("serve beer");
+
+//4
+let isStudent = "student";
+(isStudent === "student") ? console.log("Ticket costs €5,00"): console.log("Ticket costs €12,00");
+
+//5
+let okMarie = "fruits";
+(okMarie === "cake") ? console.log("Let them eat cake"): console.log("Oh, bother");
