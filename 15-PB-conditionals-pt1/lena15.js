e0c94b5416452e2e71c302beb8f28004a11e9626
LenaIT <olena.denysova@gmail.com>
2020-05-27 09:54:55 +0200

    refactoring

--- 15-PB-conditionals-pt1/lena15.js
+++ 15-PB-conditionals-pt1/lena15.js
@@ -4,16 +4,18 @@
 let intOne = 127;
 let intTwo = 68;
 
-if (((intOne >= 50) && (intOne <= 99)) || ((intTwo >= 50) && (intTwo <= 99))) {
+if ((intOne >= 50) && (intOne <= 99) || (intTwo >= 50) && (intTwo <= 99)) {
     console.log(`true, one or both variables are in the range 50 to 99`);
 } else {
     console.log(false);
 }
 
+//if((50 <= intOne <= 99) || (50 <= intTwo <= 99)) 
+
 //2
 let intThree = 155;
 
-if (((intOne >= 50) && (intOne <= 99)) || ((intTwo >= 50) && (intTwo <= 99)) || ((intThree >= 50) && (intThree <= 99))) {
+if ((intOne >= 50) && (intOne <= 99) || (intTwo >= 50) && (intTwo <= 99) || (intThree >= 50) && (intThree <= 99)) {
     console.log(`true, one or more variables are in the range 50 to 99`);
 } else {
     console.log(false);
@@ -33,19 +35,22 @@ if ((a > b) && (a > c)) {
 }
 
 //4
-let newString = "python";
+let newString = "thon";
 
-if ((newString.substring(0, 2)) === "py") {
+if (newString.substring(0, 2) === "py") {
     console.log(newString);
 } else {
     console.log(`py${newString}`);
 }
 
+//console.log(newString.startsWith('py'));    
+
 //5
 let variableOne = 35;
 let variableTwo = 65;
+let sumVariables = variableOne + variableTwo;
 
-if (((variableOne + variableTwo) >= 50) && ((variableOne + variableTwo) <= 80)) {
+if (sumVariables >= 50 && sumVariables <= 80) {
     console.log(65);
 } else {
     console.log(80);
@@ -55,7 +60,7 @@ if (((variableOne + variableTwo) >= 50) && ((variableOne + variableTwo) <= 80)) 
 let myNumber = 5;
 let myNumberTwo = 3;
 
-if (((myNumber + myNumberTwo) === 8) || ((myNumber - myNumberTwo) === 8)) {
+if ((myNumber + myNumberTwo === 8) || (myNumber - myNumberTwo === 8)) {
     console.log(true);
 } else {
     console.log(false);
@@ -66,7 +71,7 @@ if (((myNumber + myNumberTwo) === 8) || ((myNumber - myNumberTwo) === 8)) {
 let taskSeven = 12;
 let taskSevenTwo = 7;
 
-if (((taskSeven === 15) || (taskSevenTwo === 15)) || ((taskSeven + taskSevenTwo) === 15)) {
+if (taskSeven === 15 || taskSevenTwo === 15 || (taskSeven + taskSevenTwo) === 15) {
     console.log(true);
 } else {
     console.log(false);
@@ -76,13 +81,15 @@ if (((taskSeven === 15) || (taskSevenTwo === 15)) || ((taskSeven + taskSevenTwo)
 let numberMod = 65;
 let numberModTwo = 21;
 
-if ((numberMod % 7 === 0) || (numberMod % 11 === 0)) {
+if (numberMod % 7 === 0 || numberMod % 11 === 0) {
     console.log(true);
-} else if ((numberModTwo % 7 === 0) || (numberModTwo % 11 === 0)) {
+} else if (numberModTwo % 7 === 0 || numberModTwo % 11 === 0) {
     console.log(true);
 } else {
     console.log(false);
 }
+
+//if(!((numberMod % 7) || (numberMod % 11) || (numberModTwo % 7) || (numberModTwo % 11)))
 
 //9
 let numberNine = 3;
@@ -90,7 +97,7 @@ let NumberNineTwo = 3;
 let sum = numberNine + NumberNineTwo;
 
 if (numberNine === NumberNineTwo) {
-    console.log(Math.pow(sum, 3));
+    console.log(sum * 3);
 } else {
     console.log(`numbers are not the same`);
 }
@@ -101,7 +108,7 @@ let numberTenTwo = 19;
 let differ = numberTen - numberTenTwo;
 
 if (numberTen > 19) {
-    console.log(Math.pow(differ, 2));
+    console.log(differ * 2);
 } else {
     console.log(`specified number is less than 19`);
 }
