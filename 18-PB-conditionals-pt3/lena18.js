660a00356078d88fc76d121fb87e909b3228e08a
LenaIT <olena.denysova@gmail.com>
2020-05-28 13:37:48 +0200

    with two teams have a draw

--- 18-PB-conditionals-pt3/lena18.js
+++ 18-PB-conditionals-pt3/lena18.js
@@ -36,6 +36,12 @@ if (averageA > averageL && averageA > averageM) {
     console.log(`Lis\'s team is a winner, their score in average is ${averageL}`);
 } else if (averageM > averageA && averageM > averageL) {
     console.log(`Mary\'s team is a winner, their score in average is ${averageM}`);
+} else if (averageA === averageL) {
+    console.log(`Draw between A and L teams with average ${averageA}`);
+} else if (averageL === averageM) {
+    console.log(`Draw between M and L teams with average ${averageM}`);
+} else if (averageA === averageM) {
+    console.log(`Draw between M and A teams with average ${averageM}`);
 } else {
     console.log(`Teams finished three games with the same average score ${averageA}`);
 }
@@ -54,6 +60,12 @@ if (aAverageScore > lAverageScore && aAverageScore > mAverageScore) {
     console.log(`Lis\'s team is a winner, their score in average is ${lAverageScore}`);
 } else if (mAverageScore > aAverageScore && mAverageScore > lAverageScore) {
     console.log(`Mary\'s team is a winner, their score in average is ${mAverageScore}`);
+} else if (aAverageScore === lAverageScore) {
+    console.log(`Teams of Mary and Aleeza are both winners with a draw`);
+} else if (lAverageScore === mAverageScore) {
+    console.log(`Teams of Lisa and Mary are both winners with a draw`);
+} else if (aAverageScore === mAverageScore) {
+    console.log(`Teams of Aleeza and Mary are both winners with a draw`);
 } else {
     console.log(`Teams finished three games with the same average score ${averageA}`);
 }
