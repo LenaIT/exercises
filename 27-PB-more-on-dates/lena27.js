82a0d67255b5aa73441bc16dbf302a95bd54e855
LenaIT <olena.denysova@gmail.com>
2020-06-09 10:12:24 +0200

    refactoring to one function

--- 27-PB-more-on-dates/lena27.js
+++ 27-PB-more-on-dates/lena27.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// how many days in month
+/* // how many days in month
 
 function calculateDaysInMonth(year, month) {
     let monthDays = new Date(year, month + 1, 0);
@@ -18,3 +18,16 @@ function calculateDaysUntilEndOfMonth(yourDate) {
 }
 const myDate = new Date('2020-05-08');
 calculateDaysUntilEndOfMonth(myDate);
+ */
+
+//refactoring to one function
+
+function calculateDays(userDate) {
+    let days = new Date(userDate.getFullYear(), userDate.getMonth() + 1, 0);
+    let monthDays = days.getDate();
+    console.log(monthDays);
+    let untilEndMonth = monthDays - userDate.getDate();
+    console.log(`Until the end of month ${userDate.getMonth() + 1} is ${untilEndMonth} days`);
+}
+const userDate = new Date(2020, 5, 9);
+calculateDays(userDate);
