7bcbe7ca73a4b57b041576bd26c938d226618455
LenaIT <olena.denysova@gmail.com>
2020-06-15 16:05:59 +0200

    ex30 after presentation

--- 30-PB-function-closures/lena30.js
+++ 30-PB-function-closures/lena30.js
@@ -6,27 +6,47 @@ function add(number1) {
         return number1 + number2;
     }
 }
-console.log(add(3)(4));
+const sum = add(3)(4);
+console.log("//1:", sum);
 
 
 //2 Multiply
-function multiplier(number1) {
+const multiplier = function (number1) {
     return function multInside(number2) {
         return number1 * number2;
     }
 }
-let myVar = multiplier(5);
-console.log(myVar(10));
+console.log("//2:", multiplier(10)(5));
+
+
+/* function multiplier1(factor) {
+    return number => number * factor;
+}
+const twoTimes = multiplier1(2);
+const threeTimes = multiplier1(3);
+
+console.log(twoTimes(4)); //8
+console.log(threeTimes(4)); //12
+
+function multiplier(factor, number) {
+    return factor * number;
+}
+const result = multiplier(2,4);
+console.log(result); //8 */
+
 
 
 //3 Calculate Money Saved till Pension Day
-const pension = (function (age, retirementAge, salary, savingAMonth) {
+//const pension = 
+(function (age, retirementAge, salary, savingAMonth) {
     if (age >= retirementAge) {
-        console.log('You\'re already retired!');
+        console.log('//3: You\'re already retired!');
     } else {
         const restAge = retirementAge - age;
         const userSave = salary * (savingAMonth / 100);
         const result = restAge * 12 * userSave;
-        console.log(`Your savings will be ${result}\$`);
+        console.log(`//3: Your savings will be ${result}\$`);
     }
 })(40, 65, 2000, 5);
+
+//to console.log(); in the begining and function inside, and inside function return instead console.log or console.log(pension); 
