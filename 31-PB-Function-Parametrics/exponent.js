bbc6a9c8e39866ec2d54b32d6c142359f070fcaa
LenaIT <olena.denysova@gmail.com>
2020-06-16 11:32:32 +0200

    exponent without loop

--- 31-PB-Function-Parametrics/exponent.js
+++ 31-PB-Function-Parametrics/exponent.js
@@ -4,13 +4,16 @@
 
 'use strict';
 
-const exponent = (num, exp = 2) => {
+//not need a loop, we know how many arguments in function
+/* const exponent = (num, exp = 2) => {
   let result = 1;
   for (let i = 0; i < exp; i++) {
     result *= num;
   }
   return result;
-};
+}; */
+
+const exponent = (num, exp = 2) => num ** exp;
 
 console.log(exponent(3, 3)); // -> 27
 console.log(exponent(3)); // -> 9
