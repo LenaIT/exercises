68d365e7fb9bc0df1e50717d821cf53ccbb5d71d
LenaIT <olena.denysova@gmail.com>
2020-06-17 11:48:39 +0200

    array numbers of course starts from 0

--- 32-PB-datastructure-array/lena32.js
+++ 32-PB-datastructure-array/lena32.js
@@ -8,9 +8,9 @@ console.log(myArray);
 
 //2. Access the 3rd element in the array and save that to another variable named thirdElement. Print the thirdElement.
 
-const thirdElement = myArray[3];
-console.log(thirdElement); // 2020
+const thirdElement = myArray[2];
+console.log(thirdElement); // June
 
 //3. Reassign the fourth element of the array and give it a value of "fourth element". Print the array once more.
-myArray[4] = "fourth element";
+myArray[3] = "fourth element";
 console.log(myArray);
