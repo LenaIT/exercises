f5e0a1611722e039e9cb43a41f384e26231c4a62
LenaIT <olena.denysova@gmail.com>
2020-06-23 10:08:26 +0200

    after check in class

--- 35-PB-arrays-pt3/lena35.js
+++ 35-PB-arrays-pt3/lena35.js
@@ -98,18 +98,21 @@ console.log("4: ", unique([1, 1, 1, 1, 1, 1, 8])); //8
 
 
 //from Martina
-/* function unique(array) {
-    let number;
-    for (let i = 1; i < array.length; i++) {
-        if (array[i-1] !== array[i]) { // no doublette
-            number = array[i-1];
+function unique1(array) {
+    let currentValue = null;
+    for (let i = 0; i < array.length; i++) {
+        currentValue = array[i];
+        if (array.indexOf(currentValue) === array.lastIndexOf(currentValue)) { // no doublette
+            return currentValue; // uniqueness
+        } else {
+            console.log("We found a double!" + array[i]);
         }
     }
-    return number;
 }
-console.log(unique([3, 3, 3, 7, 3, 3])); // ➞ 7
-console.log(unique([0, 0, 0.77, 0, 0])); // ➞ 0.77
-console.log(unique([0, 1, 1, 1, 1, 1, 1, 1])); // ➞ 0 */
+console.log(unique1([3, 3, 3, 7, 3, 3])); // ➞ 7
+console.log(unique1([0, 0, 0.77, 0, 0])); // ➞ 0.77
+console.log(unique1([0, 1, 1, 1, 1, 1, 1, 1])); // ➞ 0
+console.log(unique1([1, 1, 1, 1, 1, 1, 1, 0])); // ➞ 0
 
 
 /* 5. Word Ranking. Create a function that takes a string of words and returns the highest scoring word. Each letter of a word scores points according to it's position in the alphabet: a = 1, b = 2, c = 3, etc.
@@ -117,7 +120,7 @@ The returned string should only contain alphabetic characters (a-z).
 Preserve case in the returned string (see 4th example above).*/
 
 function wordRank(str) {
-    const LETTERS = ['.', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    const LETTERS = ['', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     const STR_IN_ARR = str.split(' '); //separate words
     console.log(STR_IN_ARR);
 
@@ -194,6 +197,43 @@ console.log("5: ", wordRank("Today is Wednesday.")); // "Wednesday" */
 
 
 
+//from Martina
+// Word Ranking
+function wordRank1(sentence) {
+    const words = sentence.split(" ");
+    console.log(words);
+    const ranking = [];
+    for (let i = 0; i < words.length; i++) {
+        let wordScore = 0;
+        const word = words[i];
+        for (let j = 0; j < word.length; j++) {
+            const letterScore = word.toUpperCase().charCodeAt(j) - 64; // little hacky :D
+            // charCodeAt would return a number between 97 (a) - 122 (z)
+            wordScore += letterScore;
+        }
+        ranking.push(wordScore);
+    }
+    let winningPosition = 0;
+    for (let i = 0; i < ranking.length; i++) {
+        if (i === 0) {
+            // first entry, skip over
+            continue;
+        }
+        if (ranking[i] > ranking[winningPosition]) {
+            winningPosition = i;
+        }
+    }
+    return `${words[winningPosition]} is the winner with a score of ${ranking[winningPosition]}`;
+}
+
+//console.log(wordRank("Testing, one, 2, three"));
+console.log(wordRank1("The quick brown fox.")); // ➞ "brown"
+console.log(wordRank1("Nancy is very pretty.")); // ➞ "pretty"
+console.log(wordRank1("Check back tomorrow, man!")); //➞ "tomorrow"
+console.log(wordRank1("Today is Wednesday.")); // ➞ "Wednesday"
+
+// charCodeAt would return a number between 65 (A) - 90 (Z)
+
 /* 6. c4n y0u r34d th15? Create a function that takes a string as an argument and returns a coded (h4ck3r 5p34k) version of the string. NB: for your program to work properly, the function should replace all 
 'a's with 4, 
 'e's with 3, 
@@ -227,6 +267,46 @@ console.log("6: ", hackerSpeak("programming is fun")); //"pr0gr4mm1ng 15 fun"
 console.log("6: ", hackerSpeak("become a coder")); //"b3c0m3 4 c0d3r" 
 
 
+
+//from Martina 
+function hackerSpeak1(string) {
+    let arr = string.split("");
+    console.log(arr);
+    for (let i = 0; i < arr.length; i++) {
+        switch (arr[i]) {
+            case "a":
+                arr[i] = "4";
+                break;
+            case "e":
+                arr[i] = "3";
+                break;
+            case "i":
+                arr[i] = "1";
+                break;
+            case "o":
+                arr[i] = "0";
+                break;
+            case "s":
+                arr[i] = "5";
+        }
+    }
+    return arr.join("");
+}
+
+function hackerSpeak2(string) {
+    let newString = string; // copy to the string for output
+    //const regexA = /a/gi;
+    //console.log(typeof regexA);
+    newString = newString.replace(/a/gi, 4)
+        .replace(/e/gi, 3)
+        .replace(/i/gi, 1)
+        .replace(/o/gi, 0)
+        .replace(/s/gi, 5);
+    return newString;
+}
+console.log(hackerSpeak2("javascript is cool"));
+console.log(hackerSpeak2("programming is fun"));
+console.log(hackerSpeak2("become a coder"));
 // Bonus Questions
 
 /* 1. Is it Symmetrical? Create a function that takes a number as an argument and returns true or false depending on whether the number is symmetrical or not. NB: A number is symmetrical when it is the same as its reverse.
