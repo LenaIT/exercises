7781ac6f8d42fda77597f8c1fbda067081500447
LenaIT <olena.denysova@gmail.com>
2020-07-02 16:09:49 +0200

    ex 45 1,2,4

--- 45-PB-datastructure-classes/lena45.js
+++ 45-PB-datastructure-classes/lena45.js
@@ -9,7 +9,7 @@ class Person {
         this.userAge = userAge;
         this.describe = function () {
             return `${this.userName}, ${this.userAge} years old.`;
-        }
+        };
     }
 }
 
@@ -30,14 +30,19 @@ class VolumeCylinder {
     constructor(radius, height) {
         this.radius = radius;
         this.height = height;
-        this.calcVolume = function () {
+        /* this.calcVolume = function () {
             const volume = Math.PI * this.radius ** 2 * this.height;
             const result = volume.toFixed(4);
             return `The volume of your cylinder is ${result}`;
 
             //shorter
             //return (Math.PI * this.radius ** 2 * this.height).toFixed(4);
-        }
+        }; */
+    }
+    calcVolume() {
+        const volume = Math.PI * this.radius ** 2 * this.height; //hidden function, better way
+        const result = volume.toFixed(4);
+        return `The volume of your cylinder is ${result}`;
     }
 }
 
@@ -48,3 +53,68 @@ console.log(firstCylinder.calcVolume());
 const secondCylinder = new VolumeCylinder(12, 45);
 console.log(secondCylinder);
 console.log(secondCylinder.calcVolume());
+
+
+/* 4. TV Class
+Create a TV class with properties like brand, channel and volume.
+Specify brand in a constructor parameter. Channel should be 1 by default. Volume should be 50 by default.
+Add methods to increase and decrease volume. Volume can't never be below 0 or above 100.
+Add a method to set the channel randomly. Let's say the TV has only 50 channels.
+Add a method to reset TV so it goes back to channel 1 and volume 50.
+It's useful to write a status, that returns info about the TV status like: "Panasonic at channel 8, volume 75".
+*/
+
+class TV {
+    constructor(brand = "Panasonic", channel, volume) {
+        this.brand = brand;
+        this.channel = 1;
+        this.volume = 50;
+    }
+
+    increaseVolume = () => {
+        if (this.volume >= 100) {
+            console.log("Max volume reached!");
+        } else {
+            this.volume++;
+            console.log("Volume up!", this.volume);
+        }
+    }
+
+    decreaseVolume = () => {
+        if (this.volume <= 0) {
+            console.log("Min volume reached!");
+        } else {
+            this.volume--;
+            console.log("Volume Down!", this.volume);
+        }
+    }
+    setChannel(min, max) {
+        // max = 50; min = 1
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return this.channel = Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    checkStatus() {
+        return (`${this.brand} at channel ${this.channel}, volume ${this.volume}.`);
+    }
+    resetTV() {
+        this.volume = 50;
+        this.channel = 1;
+    }
+}
+
+console.log("\nTask 4:");
+const firstTV = new TV("Samsung", 5, 125);
+console.log(firstTV);
+console.log(firstTV.checkStatus());
+firstTV.increaseVolume();
+firstTV.increaseVolume();
+firstTV.increaseVolume();
+console.log(firstTV.checkStatus());
+firstTV.decreaseVolume();
+firstTV.decreaseVolume();
+console.log(firstTV.checkStatus());
+firstTV.setChannel(1, 50);
+console.log(firstTV.checkStatus());
+firstTV.resetTV();
+console.log(firstTV.checkStatus());
